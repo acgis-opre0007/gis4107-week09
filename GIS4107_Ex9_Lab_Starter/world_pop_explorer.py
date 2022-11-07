2f0e08b05bb393dc3335cb5f31bcc735ab2446c5
Nathan Mang <mang0100@algonquinlive.com>
2022-11-06 22:48:02 -0500

    Finished get_continents

--- GIS4107_Ex9_Lab_Starter/world_pop_explorer.py
+++ GIS4107_Ex9_Lab_Starter/world_pop_explorer.py
@@ -58,21 +58,46 @@ def set_country_to_pop():
         name = country_attributes[1] #key
         pop_2017 = conv_num_with_commas(country_attributes[5])
         pop_change = float(country_attributes[6].replace("%", ''))
-        country_to_pop[name] = (pop_2017, pop_change)
+        country_to_pop[name] = (pop_2017, pop_change) 
+    return country_to_pop
 
 
-def get_population(country_name):
+def get_population():
     """Given the name of the country, return the population as of 01Jul2017
        from country_to_pop.  If the country_to_pop is
        empty (i.e. no keys or values), then run set_country_to_pop
        to initialize it."""
+    
+    set_country_to_pop()
 
+    return ('Pop 01Jul2017')
+    
+    
+        
 
 def get_continents():
     """Return the list of continents"""
+    country_row = country_pop.split('\n') #every country on a line
+    
+    continent_list = []
+    
+    for row in country_row:
+        row_split = row.split('\t')
+        continent = row_split[2]
+        continent_list.append(continent)
+        
+    pop_cont_list = continent_list[1:]
 
+    unique_cont = set(pop_cont_list)
+    continents = list(unique_cont)
+    sorting = sorted(continents)
+    return (sorting)
+    
+    
 
 def get_continent_populations():
     """Returns a dict where the key is the name of the continent and
        the value is the total population of all countries on that continent"""
-
+    countries = country_pop.split('\n') #every country on a line
+    count = len(countries)
+    return count - 1
